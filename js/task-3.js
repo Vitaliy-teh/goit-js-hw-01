7055968637b230d609dea2d4bf66d7fc0379fb7e
Vitaliy_Orlov <alpachino84@gmail.com>
2025-05-31 20:19:16 +0300

    first commit

---
+++ js/task-3.js
@@ -0,0 +1,10 @@
+function getElementWidth(content, padding, border) {
+    const numContent = Number.parseFloat(content); 
+    const numPadding = Number.parseFloat(padding);
+    const numBorder = Number.parseFloat(border); 
+    return numContent + 2 * numPadding + 2 * numBorder;
+}
+
+console.log(getElementWidth("50px", "8px", "4px"));
+console.log(getElementWidth("60px", "12px", "8.5px"));
+console.log(getElementWidth("200px", "0px", "0px"));
